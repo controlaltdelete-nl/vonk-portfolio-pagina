a10e402f6be10bd2994d3286619a46a4028dd46c
Michiel Gerritsen <michielgerritsen@users.noreply.github.com>
2024-05-22 19:55:19 +0000

    Dates

--- run-database-migrations.php
+++ run-database-migrations.php
@@ -31,21 +31,21 @@ echo 'Table created' . PHP_EOL;
 $stmt = db()->prepare("INSERT INTO posts (title, description, date) VALUES (:title, :description, :date)");
 $stmt->bindValue(':title', 'Welcome Back to School!', SQLITE3_TEXT);
 $stmt->bindValue(':description', 'Excited to welcome everyone back for a great year!', SQLITE3_TEXT);
-$stmt->bindValue(':date', date('Y-m-d'), SQLITE3_TEXT);
+$stmt->bindValue(':date', '2024-05-22', SQLITE3_TEXT);
 $stmt->execute();
 
 // Insert the second record into the posts table
 $stmt = db()->prepare("INSERT INTO posts (title, description, date) VALUES (:title, :description, :date)");
 $stmt->bindValue(':title', 'Science Fair 2024', SQLITE3_TEXT);
 $stmt->bindValue(':description', 'Join us for the annual Science Fair!', SQLITE3_TEXT);
-$stmt->bindValue(':date', date('Y-m-d'), SQLITE3_TEXT);
+$stmt->bindValue(':date', '2020-04-01', SQLITE3_TEXT);
 $stmt->execute();
 
 // Insert the third record into the posts table
 $stmt = db()->prepare("INSERT INTO posts (title, description, date) VALUES (:title, :description, :date)");
 $stmt->bindValue(':title', 'Parent-Teacher Conferences', SQLITE3_TEXT);
 $stmt->bindValue(':description', 'Meet with teachers to discuss progress.', SQLITE3_TEXT);
-$stmt->bindValue(':date', date('Y-m-d'), SQLITE3_TEXT);
+$stmt->bindValue(':date', '2016-11-19', SQLITE3_TEXT);
 $stmt->execute();
 
 echo 'Dummy posts inserted' . PHP_EOL;
