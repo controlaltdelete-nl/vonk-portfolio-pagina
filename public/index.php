5c2e240112a5ac66ed135de7e5c7b17081bef61b
Michiel Gerritsen <michielgerritsen@users.noreply.github.com>
2024-05-22 10:07:45 +0000

    WIP

--- public/index.php
+++ public/index.php
@@ -1,1 +1,25 @@
-Hello world
+<?php
+
+if (!array_key_exists('REQUEST_URI', $_SERVER)) {
+    throw new \Exception('No request uri found, is this CLI?');
+}
+
+$path = ltrim($_SERVER['REQUEST_URI'], '/');
+
+if ($path == '') {
+    $path = 'index';
+}
+
+$staticPath = __DIR__ . '/' . $path;
+if (str_starts_with($path, 'assets') && file_exists($staticPath)) {
+    $mimeType = mime_content_type($staticPath);
+    header('Content-Type: ' . $mimeType);
+    readfile($staticPath);
+    exit;
+}
+
+if (!file_exists(__DIR__ . '/pages/' . $path . '.php')) {
+    throw new \Exception('404: Page not found');
+}
+
+require 'pages/' . $path . '.php';
---
+++ public/pages/aboutme.php
@@ -0,0 +1,7 @@
+<?php require(__DIR__ . '/header.php'); ?>
+
+<div class="mx-auto max-w-screen-lg px-3 py-6">
+    aboutme.
+</div>
+
+<?php require(__DIR__ . '/footer.php'); ?>
